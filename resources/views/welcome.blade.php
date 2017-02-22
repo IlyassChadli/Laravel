f8aada0e4495afa67ccaeaeac7e7cafb5628fd65
Ilyass <Ilyasschadlicandelario@gmail.com>
2017-02-22 17:15:53 +0100

    Cambio el Titular de la Pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mi Primer Proyecto Web
                 </div>
 
                 <div class="links">
