5c6432099b4c7553134f369e11603ca90c8043fc
Esteban <estebanjrr5@gmail.com>
2017-03-15 17:51:56 +0100

    Cambio el Titular de la Pagina

--- database/migrations/2017_03_06_175702_create_pacientes_table.php
+++ database/migrations/2017_03_06_175702_create_pacientes_table.php
@@ -15,6 +15,7 @@ class CreatePacientesTable extends Migration
     {
         Schema::create('pacientes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nombre');
             $table->timestamps();
 
 
