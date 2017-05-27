26393056040fb5abb61cfa30cb4fa00779f09811
Esteban <estebanjrr5@gmail.com>
2017-05-27 18:26:05 +0200

    Reordenacion y correccion

--- resources/views/Consulta/index.blade.php
+++ resources/views/Consulta/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Pagina Consulta</div>
+                <div class="panel-heading">Página Consulta</div>
 
                 <div class="panel-body">
                     @include('flash::message')
